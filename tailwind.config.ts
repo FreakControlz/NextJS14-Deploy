0b9365d378bcc4caafc25a9514e19d7adf26f53d
Florian van der Dussen <florianvanderdussen@gmail.com>
2024-08-20 22:31:50 +0200

    Fucked up and fixed css bugs in global and tailwind.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,4 @@
-import type { Config } from "tailwindcss"
-
-const config = {
-  darkMode: ["class"],
+module.exports = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
@@ -28,6 +25,13 @@ const config = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        gray: {
+          10: '#EEEEEE',
+          20: '#A2A2A2',
+          30: '#7B7B7B',
+          50: '#585858',
+          90: '#141414',
+        },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
@@ -53,7 +57,24 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      backgroundImage: {
+        'bg-img-1': "url('/Car.png')",
+        'bg-img-2': "url('/img-2.png')",
+        'blockstart': "url('/Blockstart.png')",
+        'feature-bg': "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        'pattern-2': "url('/pattern-bg.png')",
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      }, 
       borderRadius: {
+        '5xl': '40px',
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
@@ -68,6 +89,10 @@ const config = {
           to: { height: "0" },
         },
       },
+      fontFamily: {
+        'Playfair-Display': ['Playfair Display', 'serif'],
+      },
+
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
@@ -75,6 +100,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+} 
