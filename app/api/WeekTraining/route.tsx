40a3c4aa3c63ae884f41706c929d1f1b0de29870
Florian van der Dussen <florianvanderdussen@gmail.com>
2024-11-15 22:19:19 +0100

    Database temp fix

--- app/api/WeekTraining/route.tsx
+++ app/api/WeekTraining/route.tsx
@@ -7,7 +7,7 @@ let weeknumber = moment().week();
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`SELECT * FROM trainingplanning WHERE weeknummer = ${weeknumber}`;
+      await sql`SELECT * FROM trainingplanning WHERE weeknummer = 34}`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
